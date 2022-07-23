89fbbaf935409ed48e47bc2edb8ae64b0cfb5149
JK-Chung <chungjeeken@outlook.com>
2022-07-23 16:22:35 +0100

    SDOMAINS-4 Setting background color

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Header from './components/Header'
 
 function App() {
   return (
-    <div className="App dark">
+    <div className="App dark:bg-black dark">
       <Header />
     </div>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Header = () => (
-  <div className="absolute flex justify-center items-center w-screen top-0 z-50 bg-white dark:bg-sky-800 shadow-sm p-5">
+  <div className="absolute flex justify-center items-center w-screen top-0 z-50 bg-gray-300 dark:bg-slate-700 shadow-lg p-5">
     <article className="text-center prose prose-stone dark:prose-invert">
       <h1 className="font-medium m-0">SmallDomains</h1>
       <p className="text-sm m-0 mt-2">Making your domains... small</p>
