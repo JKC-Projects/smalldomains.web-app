96f38d69088307976c40313c5c3b7c142b07b909
JK-Chung <chungjeeken@outlook.com>
2022-07-23 15:51:12 +0100

    SDOMAINS-4 Tweaking header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
 const Header = () => (
-  <div className="absolute flex justify-center items-center  w-screen top-0 z-50 bg-white dark:bg-sky-800 shadow-sm p-5">
-    <article className="text-center prose prose-stone dark:prose-invert font-weigth">
-      <h1 className="m-0">SmallDomains</h1>
-      <sub className="">Making your domains... small</sub>
+  <div className="absolute flex justify-center items-center w-screen top-0 z-50 bg-white dark:bg-sky-800 shadow-sm p-5">
+    <article className="text-center prose prose-stone dark:prose-invert">
+      <h1 className="font-medium m-0">SmallDomains</h1>
+      <p className="text-sm m-0 mt-2">Making your domains... small</p>
     </article>
   </div>
 )
