271b7792d0990928f98f3c77dfde42afdbf25bc9
JK-Chung <chungjeeken@outlook.com>
2022-08-23 00:22:10 +0100

    SDOMAINS-7 Added NoSmallDomainsInfo

--- src/components/SmallDomainsDisplay/_SmallDomainsDisplayPage/_SmallDomainsDisplayPage.tsx
+++ src/components/SmallDomainsDisplay/_SmallDomainsDisplayPage/_SmallDomainsDisplayPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { SmallDomain } from '../../../types/SmallDomains'
-import _SmallDomainInfo from './_SmallDomainInfo/_SmallDomainInfo'
+import { default as NoSmallDomainInfo } from './_NoSmallDomainInfo/_NoSmallDomainInfo'
+import { default as SmallDomainInfo } from './_SmallDomainInfo/_SmallDomainInfo'
 import './_SmallDomainsDisplayPage.css'
 
 interface IProps {
@@ -11,9 +12,11 @@ const _SmallDomainsDisplayPage : React.FC<IProps> = (props) => {
   return <div className="w-[70vw]">
       <ul className="PageList">
       { 
-        props.smallDomains.map(sd => <li key={sd.smallDomain}>
-          <_SmallDomainInfo smallDomainObj={sd} />
-        </li>)
+        props.smallDomains.length === 0
+          ? <NoSmallDomainInfo />
+          : props.smallDomains.map(sd => <li key={sd.smallDomain}>
+              <SmallDomainInfo smallDomainObj={sd} />
+            </li>)
       }
     </ul>
 </div>
