9540fbb1b981ea664da4b86d3dfd9bfdccedeb5f
JK-Chung <chungjeeken@outlook.com>
2022-09-24 14:17:21 +0100

    SDOMAINS-7 Updating to use Bucket ID instead of Bucket Name

--- .terraform/output_ssm.tf
+++ .terraform/output_ssm.tf
@@ -1,6 +1,6 @@
 resource "aws_ssm_parameter" "web-app-s3" {
   name        = "/smalldomains/web-app/s3-deployment"
   type        = "String"
-  value       = aws_s3_bucket.web-app.ID
+  value       = aws_s3_bucket.web-app.id
   description = "The ID of the S3 Bucket which hosts the static site for SmallDomains"
 }
