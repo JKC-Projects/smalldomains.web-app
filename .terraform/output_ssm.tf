e4b7c61fa0464b21aad0f4bc2c338b97cfc27619
JK-Chung <chungjeeken@outlook.com>
2022-09-26 23:56:59 +0100

    SDOMAINS-7 Readding SSM Param

--- .terraform/output_ssm.tf
+++ .terraform/output_ssm.tf
@@ -1,3 +1,10 @@
+resource "aws_ssm_parameter" "web-app-cloudfront" {
+  name        = "/smalldomains/web-app/cloudfront-domain-name"
+  type        = "String"
+  value       = aws_cloudfront_distribution.web-app.domain_name
+  description = "The DNS Domain Name of the CloudFront Distribution fronting the static site for SmallDomains Web-App"
+}
+
 resource "aws_ssm_parameter" "web-app-cloudfront-domain-name" {
   name        = "/smalldomains/web-app/cloudfront-domain-name"
   type        = "String"
