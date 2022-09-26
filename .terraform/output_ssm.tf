c8324372aba5d2b62f20c9f751c123ead00c5df3
JK-Chung <chungjeeken@outlook.com>
2022-09-26 23:32:45 +0100

    SDOMAINS-7 Putting CloudFront Domain Name in SSM

--- .terraform/output_ssm.tf
+++ .terraform/output_ssm.tf
@@ -1,6 +1,6 @@
-resource "aws_ssm_parameter" "web-app-s3" {
-  name        = "/smalldomains/web-app/s3-deployment"
+resource "aws_ssm_parameter" "web-app-cloudfront" {
+  name        = "/smalldomains/web-app/cloudfront-domain-name"
   type        = "String"
-  value       = aws_s3_bucket.web-app.id
-  description = "The ID of the S3 Bucket which hosts the static site for SmallDomains"
+  value       = aws_cloudfront_distribution.web-app.domain_name
+  description = "The DNS Domain Name of the CloudFront Distribution fronting the static site for SmallDomains Web-App"
 }
