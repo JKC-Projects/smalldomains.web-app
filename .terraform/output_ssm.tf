b73c20ff2e7cd1410fdbd5616f273064b2f3b9e5
JK-Chung <chungjeeken@outlook.com>
2022-09-26 23:37:15 +0100

    SDOMAINS-7 Adding additional SSM for CloudFront's Hosted Zone ID

--- .terraform/output_ssm.tf
+++ .terraform/output_ssm.tf
@@ -1,6 +1,13 @@
-resource "aws_ssm_parameter" "web-app-cloudfront" {
+resource "aws_ssm_parameter" "web-app-cloudfront-domain-name" {
   name        = "/smalldomains/web-app/cloudfront-domain-name"
   type        = "String"
   value       = aws_cloudfront_distribution.web-app.domain_name
   description = "The DNS Domain Name of the CloudFront Distribution fronting the static site for SmallDomains Web-App"
+}
+
+resource "aws_ssm_parameter" "web-app-cloudfront-hosted-zone-id" {
+  name        = "/smalldomains/web-app/cloudfront-hosted-zone-id"
+  type        = "String"
+  value       = aws_cloudfront_distribution.web-app.hosted_zone_id
+  description = "The Hosted Zone ID of the CloudFront Distribution fronting the static site for SmallDomains Web-App"
 }
