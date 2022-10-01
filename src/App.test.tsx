33130328cba963767b37faa215c52ca3c2938f0f
JK-Chung <chungjeeken@outlook.com>
2022-10-01 20:42:15 +0100

    SDOMAINS-7 Small update to tech stack info in header

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -10,6 +10,6 @@ test('does subtitle exist', () => {
 
 test('does tech stack info exist', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Powered by AWS, Typescript, React, Go, Java, Terraform, GitHub Actions/i);
+  const linkElement = screen.getByText(/Powered by AWS, Typescript, React, Go, Java \(Spring Webflux\), Terraform, GitHub Actions/i);
   expect(linkElement).toBeInTheDocument();
 });
