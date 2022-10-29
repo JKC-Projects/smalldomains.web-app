568ea7d227a6c4dfb18254d04d8c5b06461df040
JK-Chung <chungjeeken@outlook.com>
2022-10-29 18:03:14 +0100

    SDOMAINS-9 adding SSM param of the domain-name of the hosted-ui

--- src/types/environment.d.ts
+++ src/types/environment.d.ts
@@ -3,11 +3,11 @@ export {};
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      NEXT_PUBLIC_SMALL_DOMAINS_REST_API_BASE_URL : string;
-      NEXT_PUBLIC_SMALL_DOMAINS_FORWARDER_USER_FRIENDLY_URL : string;
+      NEXT_PUBLIC_SMALL_DOMAINS_REST_API_BASE_URL : string
+      NEXT_PUBLIC_SMALL_DOMAINS_FORWARDER_USER_FRIENDLY_URL : string
 
-      NEXT_PUBLIC_OAUTH2_SERVICE_ORIGIN : string;
-      NEXT_PUBLIC_OAUTH2_CLIENT_ID : string;
+      NEXT_PUBLIC_OAUTH2_SERVICE_ORIGIN : string
+      NEXT_PUBLIC_OAUTH2_CLIENT_ID : string
     }
   }
 }
